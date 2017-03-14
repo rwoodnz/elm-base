068ee65e64f42cad1e330d528bde62cfb1082466
Richard Wood <rwoodnz@gmail.com>
2017-03-15 00:30:58 +1300

    Fix test file

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,26 +1,25 @@
--- Sample tests
+module Tests exposing (..)
 
--- module Tests exposing (..)
-
--- import Test exposing (..)
--- import Expect
--- import String
+import Test exposing (..)
+import Expect
+import String
 -- import App
 
 
--- all : Test
--- all =
---     describe "A Test Suite"
---         [ test "App.model.message should be set properly" <|
---             \() ->
---                 Expect.equal (Tuple.first (App.init "") |> .message) "Base Elm App"
---         , test "App.model.path should be set properly" <|
---             \() ->
---                 Expect.equal (Tuple.first (App.init "logo-path") |> .logo) "logo-path"
---         , test "Addition" <|
---             \() ->
---                 Expect.equal (3 + 7) 10
---         , test "String.left" <|
---             \() ->
---                 Expect.equal "a" (String.left 1 "abcdefg")
---         ]
+all : Test
+all =
+    describe "A Test Suite"
+        [ 
+        -- test "App.model.message should be set properly" <|
+        --     \() ->
+        --         Expect.equal (Tuple.first (App.init "") |> .message) "Base Elm App"
+        -- , test "App.model.path should be set properly" <|
+        --     \() ->
+        --         Expect.equal (Tuple.first (App.init "logo-path") |> .logo) "logo-path"
+        test "Addition" <|
+            \() ->
+                Expect.equal (3 + 7) 10
+        , test "String.left" <|
+            \() ->
+                Expect.equal "a" (String.left 1 "abcdefg")
+        ]
